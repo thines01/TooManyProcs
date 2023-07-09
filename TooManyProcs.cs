6ea069328bf9bf680b8b01ea80ea6a4e2ffa6b30
thines01 <thines01@yahoo.com>
2023-07-09 13:02:27 -0500

    Add project files.

---
+++ TooManyProcs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace TooManyProcs
+{
+   public class CTooManyProcs
+   {
+      public static bool TooMany(string strProcName, int intHowManyOk)
+      {
+         return Process.GetProcesses().Where(proc => proc.ProcessName.Equals(strProcName, StringComparison.CurrentCultureIgnoreCase)).Count() >= intHowManyOk;
+      }
+
+      public static bool TooMany(Regex rxProcName, int intHowManyOk)
+      {
+         return Process.GetProcesses().Where(proc => rxProcName.IsMatch(proc.ProcessName)).Count() >= intHowManyOk;
+      }
+
+      public static string GetMyProcName()
+      {
+         return Process.GetCurrentProcess().ProcessName;
+      }
+   }
+}
